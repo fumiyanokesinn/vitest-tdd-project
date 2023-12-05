5caaf86148c134802a1e3feb82739dc93b3ba81c
shindori fumiya <shindori238@gmail.com>
2023-12-05 23:49:33 +0900

    create project

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  // typescriptの設定
+  typescript: {
+    shim: false, // shimsファイル生成の無効化（VSCodeでVolarを使う場合はfalseにする）
+    strict: true, // 型チェックを厳格化
+    typeCheck: true, // nuxt devまたはnuxt build時に型チェックを実行
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const name = ref('world');
+</script>
+
+<template>
+  <div>
+    <HelloMessage :name="name" />
+    <p>
+      <input v-model="name" type="text" />
+    </p>
+  </div>
+</template>
