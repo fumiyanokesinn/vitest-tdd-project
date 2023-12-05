430d0c84b64c6eda56809c93ed31977e919bd29b
shindori fumiya <shindori238@gmail.com>
2023-12-06 00:16:21 +0900

    vitest 導入

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import Vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+  },
+});
