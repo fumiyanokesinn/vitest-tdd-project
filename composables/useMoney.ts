4cc186bfed3f26a31e57ea2c2d204ac49ee8046a
shindori fumiya <shindori238@gmail.com>
2023-12-08 23:35:24 +0900

    moneyのテスト作成

--- composables/useMoney.ts
+++ composables/useMoney.ts
@@ -1,5 +1,25 @@
 interface Money {
-  amount: number;
+  amount: Ref<number>;
   currency: string;
-  times: () => void;
+  times: (number: number) => void;
 }
+
+/**
+ * Moneyを取得する
+ * @param number
+ * @returns {Money}
+ */
+const useMoney = (number: number): Money => {
+  const amount = ref(number);
+
+  /**
+   * 金額を乗算する
+   * @param number
+   */
+  const times = (timesNumber: number) => {
+    amount.value *= timesNumber;
+  };
+  return { amount, currency: '', times };
+};
+
+export default useMoney;
--- test/composables/useMoney.spec.ts
+++ test/composables/useMoney.spec.ts
@@ -1,10 +1,10 @@
 import { describe, test, expect } from 'vitest';
+import useMoney from '../../composables/useMoney';
 
 describe('useMoney', () => {
-  test('掛け算が正しい'),
-    () => {
-      const { amount, times } = useMoney(10);
-      times(2);
-      expect(amount).to.equal(20);
-    };
+  test('掛け算が正しい', () => {
+    const { amount, times } = useMoney(10);
+    times(2);
+    expect(amount.value).to.equal(20);
+  });
 });
