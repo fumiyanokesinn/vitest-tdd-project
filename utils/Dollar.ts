48b6ca4c69ebc990d1659200e1e30c98921760e2
shindori fumiya <shindori238@gmail.com>
2023-12-15 23:58:25 +0900

    utilsファイル作成

---
+++ utils/Dollar.ts
@@ -0,0 +1,10 @@
+class Dollar extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
+  }
+}
---
+++ utils/Money.ts
@@ -0,0 +1,11 @@
+class Money {
+  amount: number;
+
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  times(multiplier: number) {
+    this.amount *= multiplier;
+  }
+}
